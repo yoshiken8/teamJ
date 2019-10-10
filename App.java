9f9cfa2a1a9ff5fa1381998f866d0a9f671d9644
yoshiken8 <yoshikenyoshi8@gmail.com>
2019-10-10 16:42:03 +0900

    Car

--- App.java
+++ App.java
@@ -28,6 +28,53 @@ class Train{
         fuel=10;
     }
 }
+
+class Car
+{
+	private int mileage;	// 走行距離
+	private int fuel;	// ガソリン量
+
+	public Car( )
+	{
+		mileage = 0;
+		fuel = 10;
+	}
+	public void goForward( )
+	{
+		mileage += 1;
+		fuel -= 1;
+	}
+	public void turnLeft( )
+	{
+		mileage += 1;
+		fuel -= 2;
+	}
+	public void turnRight( )
+	{
+		mileage += 1;
+		fuel -= 2;
+	}
+	public int getFuel( )
+	{
+		return fuel;
+	}
+	public void refuel( )
+	{
+		fuel = 10;	// ガソリン満タン（=10）
+	}
+	public int getMileage( )
+	{
+		return mileage;
+	}
+	public void setMileage( int m )
+	{
+		if( 0 <= m && m <= 20 )
+		{
+			mileage = m;
+		}
+	}
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("乗りたい乗り物");
