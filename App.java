c50c0d720796d888ec94d5a1f853858f7667ba49
tanaseyurina <yurinax529@gmail.com>
2019-10-10 16:42:10 +0900

    Bike

--- App.java
+++ App.java
@@ -28,6 +28,36 @@ class Train{
         fuel=10;
     }
 }
+
+
+class Bike{
+    int mileageX;
+    int mileageY;
+    int mileageZ;
+    int fuel;
+    public Bike()
+    {
+        mileageX=0;
+        mileageY=0;
+        mileageZ=0;
+        fuel=10;
+    }
+    public void Run(){
+        mileageY+=2;
+        fuel-=1;
+    }
+    public void Jump(){
+        mileageZ+=1;
+        fuel-=1;
+    }
+    public void RunAround(){
+        mileageX+=3;
+        fuel-=2;
+    }
+    public void refuel(){
+        fuel=10;
+    }
+}
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("乗りたい乗り物");
